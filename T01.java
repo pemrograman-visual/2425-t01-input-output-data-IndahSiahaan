7e1dce234322f84342638dddd70f7fcb9848d8c7
IndahSiahaan <triyunisiahaan@gmail.com>
2024-09-06 11:27:38 +0700

    sudah menyelesaikan persoalan

--- T01.java
+++ T01.java
@@ -1,1 +1,34 @@
-// NIM - Your Name
+// 12S24052 - Indah Triyuni Siahaan
+// 12S24007 - Dasmauli Sormin
+
+import java.util.*;
+import java.lang.Math;
+
+public class Program {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String iSBN;
+        String judul;
+        String penulis;
+        int tahunTerbit;
+        String penerbit;
+        String format;
+        double harga;
+        double minimumMargin;
+        int stok;
+        double rating;
+
+        iSBN = input.nextLine();
+        judul = input.nextLine();
+        penulis = input.nextLine();
+        tahunTerbit = input.nextInt();
+        penerbit = input.nextLine();
+        format = input.nextLine();
+        harga = input.nextDouble();
+        minimumMargin = input.nextDouble();
+        stok = input.nextInt();
+        rating = input.nextDouble();
+        System.out.println(iSBN + "|" + judul + "|" + penulis + "|" + tahunTerbit + "|" + penerbit + "|" + format + "|" + harga + "|" + minimumMargin + "|" + stok + "|" + rating);
+    }
+}
